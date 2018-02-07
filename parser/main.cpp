bc1ccdc0feb07ce3b2bb464c623617e216cc5be8
Evgeny Britan <evgenybritan@gmail.com>
2018-02-07 15:08:31 +0300

    git add readme

--- parser/main.cpp
+++ parser/main.cpp
@@ -78,52 +78,93 @@ int main() {
     case 0x01:
         pflag=Packet_type(HB0);
         cout<<"packet type: "<<arr_type[pflag]<<endl;
-        if ((HB1 == 0) && (HB2 == 0) && (SB0 == 0)){
+        if ((HB1 == 0) && (HB2 == 0) && (SB0 == 0)&& ((SB1>>4) == 0)){
             long CTS = (SB1<<16)|(SB2<<8)|SB3;
             long N = (SB4<< 16)|(SB5<<8)|SB6;
             cout<<"CTS: "<< CTS <<endl;
             cout<<"N: "<< N <<endl;
         }
+        else{
+            cout<<"invalid"<<endl;
+        }
         break;
     case 0x02:
         pflag=Packet_type(HB0);
         cout<<"packet type: "<<arr_type[pflag]<<endl;
-        if (HB0 == 0x02){
-              long layout = (HB1>>4);
-              long sp0 = (HB1<<0)|(HB1<<1)|(HB1<<2)|(HB1<<3);
-              long sp1 = (HB1<<4)>>4;
-              long sp2 = (HB1>>2);
-              long sp3 = (HB1>>3);
-              long sf0 = (HB2>>0);
-              long sf1 = (HB2>>1);
-              long sf2 = (HB2>>2);
-              long sf3 = (HB2>>3);
-              long B0 = (HB2>>4);
-              long B1 = (HB2>>5);
-              long B2 = (HB2>>6);
-              long B3 = (HB2>>7);
+        if ((HB1>>5) == 0){
+              long layout = HB1&0x10;
+              long spX = HB1&0xF;
+              long sfX = HB2&0xF;
+              long B = HB2&(0xF0);
+              long L = (SB0<<4)|(SB1<<12)|(SB2<<20);
+              long R = (SB3<<4)|(SB4<<12)|(SB5<<20);
+              long Vl =(SB6&0x01);
+              long Vr =(SB6&0x10);
+              long Ul =(SB6&0x02);
+              long Ur =(SB6&0x20);
+              long Cl =(SB6&0x04);
+              long Cr =(SB6&0x40);
+              long Pl =(SB6&0x08);
+              long Pr =(SB6&0x80);
+
               cout<<"layout: "<<layout<<endl;
-              cout<<"sample_present.sp0: "<<sp0<<endl;
-              cout<<"sample_present.sp1: "<<sp1<<endl;
-              cout<<"sample_present.sp2: "<<sp2<<endl;
-              cout<<"sample_present.sp3: "<<sp3<<endl;
-              cout<<"sample_flat.sp0: "<<sf0<<endl;
-              cout<<"sample_flat.sp1: "<<sf1<<endl;
-              cout<<"sample_flat.sp2: "<<sf2<<endl;
-              cout<<"sample_flat.sp3: "<<sf3<<endl;
-              cout<<"B: "<<B0<<endl;
-              cout<<"B: "<<B1<<endl;
-              cout<<"B: "<<B2<<endl;
-              cout<<"B: "<<B3<<endl;
+              cout<<"sample_present.spX: "<<spX<<endl;
+              cout<<"sample_flat.spX: "<<sfX<<endl;
+              cout<<"B: "<<B<<endl;
+              cout<<"L: "<<L<<endl;
+              cout<<"R: "<<R<<endl;
+              cout<<"Vl: "<<Vl<<endl;
+              cout<<"Vr: "<<Vr<<endl;
+              cout<<"Ul: "<<Ul<<endl;
+              cout<<"Ur: "<<Ur<<endl;
+              cout<<"Cl: "<<Cl<<endl;
+              cout<<"Cr: "<<Cr<<endl;
+              cout<<"Pl: "<<Pl<<endl;
+              cout<<"Pr: "<<Pr<<endl;
          }
+        else{
+            cout<<"invalid"<<endl;
+        }
         break;
     case 0x03:
         pflag=Packet_type(HB0);
         cout<<"packet type: "<<arr_type[pflag]<<endl;
+         if ((HB1 == 0) && (HB2 == 0) && ((SB0&0xEE)==0) && ((SB2>>1)==0) && (SB3 == 0) && (SB4 == 0) && (SB5 == 0) && (SB6 == 0)){
+             long setAVMUTE = (SB0&0x01);
+             long clearAVMUTE = (SB0&0x10);
+             long PP = (SB1)&(0xF0);
+             long CD = (SB1&0xF);
+             long defaultPhase = (SB2&0x01);
+             cout<<"Set_AVMUTE: "<<setAVMUTE<<endl;
+             cout<<"Clear_AVMUTE: "<<clearAVMUTE<<endl;
+             cout<<"PP: "<<PP<<endl;
+             cout<<"CD: "<<CD<<endl;
+             cout<<"Default_Phase: "<<defaultPhase<<endl;
+         }
         break;
     case 0x04:
         pflag=Packet_type(HB0);
         cout<<"packet type: "<<arr_type[pflag]<<endl;
+        if (HB0 == 0x04){
+            switch (HB1) {
+            case 0x00:
+                cout<<"ACP_Type: Generic Audio"<<endl;
+                break;
+            case 0x01:
+                cout<<"ACP_Type: IEC 60958-Identified Audio"<<endl;
+                break;
+            case 0x02:
+                cout<<"ACP_Type: DVD-Audio"<<endl;
+                break;
+            case 0x03:
+                cout<<"ACP_Type: Super Audio CD"<<endl;
+                break;
+            default:
+                cout<<"*reserved*"<<endl;
+                break;
+            }
+
+        }
         break;
     case 0x05:
         pflag=Packet_type(HB0);
